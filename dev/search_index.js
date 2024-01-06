e2a86ac6e663562f5979090795f4d0a9d6ad2aef
Documenter.jl <documenter@juliadocs.github.io>
2024-01-06 19:10:25 +0000

    build based on d703f70

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MTH229Lite","category":"page"},{"location":"#MTH229Lite","page":"Home","title":"MTH229Lite","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MTH229Lite.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MTH229Lite]","category":"page"}]
+}
